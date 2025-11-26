6bf79b549cff502c13c2def02f3bd9984b2af406
leonshimizu <lmshimizu@gmail.com>
2025-11-26 21:04:55 +1000

    Fix timezone to use Guam time (ChST, UTC+10)
    - Set config.time_zone to 'Guam' in application.rb
    - Update admin notification email to explicitly use Guam timezone for timestamps

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module GiaaTournamentApi
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    # Set timezone to Guam (Chamorro Standard Time - UTC+10)
+    config.time_zone = "Guam"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
